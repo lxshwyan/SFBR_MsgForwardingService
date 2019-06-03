80171ec6376ed3bf0b4fa277fc697a24fc463581
liguodong <admin@DESKTOP-VI4T4MH>
2019-06-03 17:04:12 +0800

    新增AB门复核消息

--- SFBR_MsgForwardingService/AppStart/Startup.cs
+++ SFBR_MsgForwardingService/AppStart/Startup.cs
@@ -15,7 +15,7 @@
 using SFBR_MsgForwardingService.Hub;
 using System;
 using System.Reflection;
-using System.Threading;  
+using System.Threading;
 using Microsoft.AspNet.SignalR;
 using Owin;
 using Microsoft.Owin.Cors;
@@ -28,11 +28,13 @@ namespace SFBR_MsgForwardingService
 
     public class Startup
     {
-      
-        public string SinalRUrl { get; set; } ="http://localhost:6178";
+
+        public string SinalRUrl { get; set; } = "http://localhost:8893";
         public int LinsenPort { get; set; } = 8893;
-        public  Logger logger = LogManager.GetLogger(nameof(Startup));
-        private  void InitSinalR(string SignalRURI = "http://localhost:6178")
+        public int ABDoorPort { get; set; } = 8887;
+
+        public Logger logger = LogManager.GetLogger(nameof(Startup));
+        private void InitSinalR(string SignalRURI = "http://localhost:8893")
         {
             try
             {
@@ -41,33 +43,33 @@ namespace SFBR_MsgForwardingService
                     using (WebApp.Start(SignalRURI, builder =>
                     {
                         builder.Map("/signalr", map =>
-                        {  
-                            map.UseCors(CorsOptions.AllowAll);
+                        {
+                            map.UseCors(CorsOptions.AllowAll);   //运行跨域
                             var hubConfiguration = new HubConfiguration
-                            { 
+                            {
                                 EnableJSONP = true
-                            };  
+                            };
                             map.RunSignalR(hubConfiguration);
                         });
                         builder.MapSignalR();
 
                     }))
-                    {    
+                    {
                         logger.Info("服务开启成功,运行在{0}", SignalRURI + "/signalr");
                         Console.ReadLine();
                     }
                 }
                 catch (TargetInvocationException)
                 {
-                    
-                   logger.Info("服务开启失败. 已经有一个服务运行在{0}", SignalRURI);
+
+                    logger.Info("服务开启失败. 已经有一个服务运行在{0}", SignalRURI);
                     Console.ReadLine();
                 }
             }
             catch (Exception ex)
             {
                 logger.Info("服务开启异常：{0}", ex.ToString());
-              
+
                 Console.ReadLine();
             }
         }
@@ -85,13 +87,17 @@ namespace SFBR_MsgForwardingService
                 {
                     LinsenPort = int.Parse(System.Configuration.ConfigurationManager.AppSettings["LinsenPort"].ToString());
                 }
+                if (!string.IsNullOrWhiteSpace(System.Configuration.ConfigurationManager.AppSettings["ABDoorPort"]))
+                {
+                    ABDoorPort = int.Parse(System.Configuration.ConfigurationManager.AppSettings["ABDoorPort"].ToString());
+                }
             }
             catch (Exception ex)
             {
-                logger.Error(ex,"读取参数错误");
-                
+                logger.Error(ex, "读取参数错误");
+
             }
-            
+
 
         }
         /// <summary>
@@ -100,11 +106,12 @@ namespace SFBR_MsgForwardingService
         public void Start()
         {
             ReadConfig();
-             PlatAlarmMsg._Instace.InitUdp(LinsenPort); 
+            PlatAlarmMsg._Instace.InitUdp(LinsenPort);
+            ABDoorMsg._Instace.InitUdp(ABDoorPort);
 
-             InitSinalR(SinalRUrl);
+            InitSinalR(SinalRUrl);
 
-         
+
         }
         /// <summary>
         /// 停止服务
@@ -113,7 +120,7 @@ namespace SFBR_MsgForwardingService
         {
 
             PlatAlarmMsg._Instace.Dis();
-             logger.Info("已停止服务");
+            logger.Info("已停止服务");
         }
     }
 }
---
+++ SFBR_MsgForwardingService/DoorMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFBR_MsgForwardingService
+{
+    public class DoorMessage
+    {
+        public int AssertTypeID;
+
+        public int DevType { get; set; }
+        public string AssertGroupID { get; set; }
+        public int TypeID { get; set; }
+        public string Expression { get; set; }
+        public string JY { get; set; }
+        public string Dept { get; set; }
+        public string Photo { get; set; }
+        public string DateTime { get; set; }
+        public string Address { get; set; }
+        public string CardID { get; set; }
+        public string AssertName { get; set; }
+        public string AssertID { get; set; }
+        public string DoorID { get; set; }
+        public string StatuesID { get; set; }
+        public int CheckResult { get; set; }
+    }
+}
